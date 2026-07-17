de354e41e505610d294c43364ac0aca4dbb3c54e
Natalia Kobeleva <cobeleva.natasha@gmail.com>
2026-07-17 10:39:41 +0400

    Add SonarCloud workflow and CLI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.github.ben-manes.versions")  version "0.52.0"
+    id("org.sonarqube") version "7.3.1.8318"
     application
 }
 
@@ -18,6 +19,17 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "NatKobeleva_qa-auto-engineer-java-project-61")
+        property("sonar.organization", "natkobeleva")
+    }
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
 }
 
 tasks.test {
